ecd8624d3e08a258b498068f73ce6840fb7239a4
PrinceMargaret <princeagrahari2000@gmail.com>
2022-07-22 10:46:16 +0530

    update

---
+++ routes/playListRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { getPlayLists, AddSongToPlayList,createPlaylist,getSinglePlayList,deletePlaylist } = require('../controllers/PlayListController');
+
+
+router.get('/getPlaylist', getPlayLists);
+router.get("/getPlaylist/:id", getSinglePlayList);
+router.post('/createPlaylist',createPlaylist );
+router.post('/addSongToPlaylist/:playlistId', AddSongToPlayList);
+router.delete('/deletePlaylist/:id', deletePlaylist);
+
+
+
+module.exports = router;
